9f5af53ab0f0fd51fa067a1d2b8d1d84428483d3
Krishin Parikh <krishinparikh@gmail.com>
2025-02-06 20:36:58 +0100

    More changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,13 +6,8 @@ const Navbar: React.FC = () => {
   return (
     <nav className={styles.navbar}>
       <Link to="/" className={styles.navLink}>HOME</Link>
-<<<<<<< Updated upstream
-      <Link to="/projects" className={styles.navLink}>PROJECTS</Link>
-      <Link to="/blog" className={styles.navLink}>BLOG</Link>
-=======
       <Link to="/portfolio" className={styles.navLink}>PORTFOLIO</Link>
       <Link to="https://www.linkedin.com/in/krishin-parikh/recent-activity/all/" target="_blank" className={styles.navLink}>BLOG</Link>
->>>>>>> Stashed changes
     </nav>
   );
 };
